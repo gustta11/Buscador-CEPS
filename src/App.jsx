bda4d3d337dd24d90b57f9f3cc2f297cb10ae4a8
gustta11 <gustavokeven50@gmail.com>
2024-03-28 19:12:14 -0300

    Modificando arquivo Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import PesquisarCep from './components/BuscaCep'
-
+import NavBar from './layout/Nav.jsx'
 function App() {
   
 
   return (
     <div className='grid'>
-      <div className='header'>
 
+      <div className='header'>
+         <NavBar/>
       </div>
 
       <div className='main'>
--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,7 +1,14 @@
 import './Nav.css'
 
-const Nav = ()=>{
+const NavBar = ()=>{
 
+    return(
+    <div className='div_geral_nav'>
+        <div className='div_titulo_nav'>
+            <h1>BUSCA DE ENDEREÇOS</h1>
+        </div>
+    </div>
+    )
 }
 
-export default Nav
+export default NavBar
